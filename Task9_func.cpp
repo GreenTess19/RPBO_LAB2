2cd4b96a4894a0744c620226d6b9a523d0589ac1
GreenTess19 <90135565+GreenTess19@users.noreply.github.com>
2022-01-21 21:50:10 +0300

    Update Task9_func.cpp

--- Task9_func.cpp
+++ Task9_func.cpp
@@ -1,10 +1,10 @@
-#include <iostream>
-#include "func.h"
+#include "kiseleva.h"
+using namespace std;
 
 double x = 10;
 double result = 0;
 
-namespace Kiseleva {
+namespace kiseleva {
 	void f() {
 		result = (1 - 2 * (sin(x) * sin(x))) / (1 + sin(2 * x));
 	}
